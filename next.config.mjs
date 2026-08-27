77f152c9bbfb19fbd8641c28e118eae29f5ea54f
thegardenedition <nachangho35@gmail.com>
2026-08-27 16:56:39 +0900

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+          remotePatterns: [
+            { protocol: 'https', hostname: 'images.unsplash.com' },
+            { protocol: 'https', hostname: 'cdn.prod.website-files.com' },
+            { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+                ],
+    },
+};
+
+export default nextConfig;
